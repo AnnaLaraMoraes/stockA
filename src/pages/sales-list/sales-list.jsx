0bd4fb6e5b52ca84fd7ec7a1ba1c9c064e384f75
Anna Lara Moraes Caixeta <annalara1426@gmail.com>
2022-07-28 15:45:59 +0200

    adjustments in sales

--- src/pages/sales-list/sales-list.jsx
+++ src/pages/sales-list/sales-list.jsx
@@ -1,6 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
-import { MdDeleteForever, MdModeEdit } from 'react-icons/md';
+import {
+  MdDeleteForever,
+  MdModeEdit,
+  MdOutlineAddCircle,
+  MdDoneAll,
+  MdRemoveDone,
+} from 'react-icons/md';
 import { useHistory } from 'react-router-dom';
 import Layout from '../layouts';
 import api from '../../services/api';
@@ -12,12 +18,35 @@ function SalesList() {
   const history = useHistory();
   const [sales, setSales] = useState([]);
   const [loading, setLoading] = useState(false);
+  const inputPaymentRef = useRef([]);
+  const inputDataRef = useRef([]);
 
   const findSales = async () => {
     try {
       setLoading(true);
       const { data } = await api.get('/sales');
-      setSales(data && data.sales ? data.sales : []);
+      if (data && data.sales) {
+        const dataFormated = data.sales.map((dataSale) => {
+          const { totalValuePaid } = dataSale.received.reduce(
+            (accumulator, receveidData) => {
+              accumulator.totalValuePaid += Number(receveidData.value);
+
+              return accumulator;
+            },
+            {
+              totalValuePaid: 0,
+            }
+          );
+          dataSale.totalValuePaid = totalValuePaid;
+          dataSale.itsPaid =
+            Number(totalValuePaid) >= Number(dataSale.totalValue);
+
+          return dataSale;
+        });
+        setSales(dataFormated);
+      } else {
+        setSales([]);
+      }
       setLoading(false);
     } catch (error) {
       toast.error(
@@ -43,6 +72,28 @@ function SalesList() {
     findSales();
   };
 
+  const onButtonClick = async (saleId, index) => {
+    if (inputPaymentRef.current[index].value && saleId) {
+      try {
+        await api.put(`/sales-payments/${saleId}`, {
+          received: {
+            value: inputPaymentRef.current[index].value,
+            date: inputDataRef.current[index].value || new Date(),
+          },
+        });
+        toast.success('Pagamento adicionado com sucesso!');
+      } catch (error) {
+        toast.error('Erro ao adicionar pagamento, tente mais tarde.');
+      } finally {
+        inputPaymentRef.current[index].value = null;
+        inputDataRef.current[index].value = null;
+        findSales();
+      }
+    } else {
+      toast.error('Adicione o valor do pagamento');
+    }
+  };
+
   return (
     <Layout>
       <Layout.Content title="Vendas">
@@ -55,19 +106,63 @@ function SalesList() {
           ) : (
             <table>
               <tr>
+                <th>Status</th>
                 <th>Data</th>
                 <th>Cliente</th>
                 <th>Valor total</th>
+                <th>Valor Pago</th>
+                <th>Adicionar pagamento</th>
                 <th>Editar</th>
                 <th>Exluir</th>
               </tr>
 
               {sales.length > 0 &&
-                sales.map((sale) => (
+                sales.map((sale, i) => (
                   <tr key={sale._id}>
+                    <td>
+                      <div>
+                        {sale.itsPaid ? (
+                          <>
+                            <MdDoneAll style={{ color: '#388E3C' }} />
+                            Pago
+                          </>
+                        ) : (
+                          <>
+                            <MdRemoveDone style={{ color: '#D32F2F' }} />
+                            Não pago
+                          </>
+                        )}
+                      </div>
+                    </td>
                     <td>{new Date(sale.date).toLocaleDateString()}</td>
                     <td>{sale.client.name}</td>
                     <td>{`R$${sale?.totalValue}`}</td>
+                    <td>{`R$${sale?.totalValuePaid}`}</td>
+                    <td>
+                      <div className={style.AddPayment}>
+                        <input
+                          type="number"
+                          min="1"
+                          ref={(el) => {
+                            inputPaymentRef.current[i] = el;
+                            return el;
+                          }}
+                        />
+                        <input
+                          type="date"
+                          ref={(el) => {
+                            inputDataRef.current[i] = el;
+                            return el;
+                          }}
+                        />
+                        <button
+                          type="button"
+                          onClick={() => onButtonClick(sale._id, i)}
+                        >
+                          <MdOutlineAddCircle style={{ color: '#388E3C' }} />
+                        </button>
+                      </div>
+                    </td>
                     <td>
                       <button onClick={() => handleEdit(sale)} type="button">
                         <MdModeEdit className={style.EditButton} />
